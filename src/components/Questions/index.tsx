67311ee3c4d4e61e3f2b247fda2646612a2695e7
nosilaepilef <nosilaepilef@gmail.com>
2021-06-28 14:29:21 -0400

    Application in operation more under development

---
+++ src/components/Questions/index.tsx
@@ -0,0 +1,50 @@
+import { ReactNode } from "react"
+import cx from "classnames"
+
+import "../Questions/styles.scss"
+
+type QuestionsProps = {
+  content: string;
+  author: {
+    name: string;
+    avatar: string;
+  }
+
+  children?: ReactNode;
+  isAnswered?: boolean;
+  isHighlighted?: boolean;
+}
+
+export function Question({ 
+  content,
+  author,
+  children,
+  isAnswered = false,
+  isHighlighted = false,
+ }: QuestionsProps) {
+  return (
+    // Classname de forma padrão
+    // <div 
+    //   className={`question ${isAnswered ? "answered" : ""} ${isHighlighted ? "highlighted" : ""}`}
+    // >
+    // Classname de forma com biblioteca "classnames"
+    <div 
+      className={cx(
+        `question`,
+        { answered: isAnswered },
+        { highlighted: isHighlighted && !isAnswered },
+      )}
+    >
+      <p>{ content }</p>
+      <footer>
+        <div className="user--info">
+          <img src={ author.avatar } alt={ author.name } />
+          <span>{ author.name }</span>
+        </div>
+        <div>
+          {children}
+        </div>
+      </footer>
+    </div>
+  )
+}
